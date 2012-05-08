31e36ecc1b9efbd3463500a6254834cd50bc384c
Aleksandar Gargenta <sasa@gargenta.com>
2012-05-08 01:41:31 -0700

    switched to SystemClock

--- src/com/marakana/android/fibonaccinative/FibonacciActivity.java
+++ src/com/marakana/android/fibonaccinative/FibonacciActivity.java
@@ -1,10 +1,10 @@
-
 package com.marakana.android.fibonaccinative;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,60 +15,61 @@ import android.widget.TextView;
 
 public class FibonacciActivity extends Activity implements OnClickListener {
 
-    private EditText input;
+	private EditText input;
 
-    private RadioGroup type;
+	private RadioGroup type;
 
-    private TextView output;
+	private TextView output;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        this.input = (EditText) super.findViewById(R.id.input);
-        this.type = (RadioGroup) super.findViewById(R.id.type);
-        this.output = (TextView) super.findViewById(R.id.output);
-        Button button = (Button) super.findViewById(R.id.button);
-        button.setOnClickListener(this);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		this.input = (EditText) super.findViewById(R.id.input);
+		this.type = (RadioGroup) super.findViewById(R.id.type);
+		this.output = (TextView) super.findViewById(R.id.output);
+		Button button = (Button) super.findViewById(R.id.button);
+		button.setOnClickListener(this);
+	}
 
-    public void onClick(View view) {
-        String s = this.input.getText().toString();
-        if (TextUtils.isEmpty(s)) {
-            return;
-        }
+	public void onClick(View view) {
+		String s = this.input.getText().toString();
+		if (TextUtils.isEmpty(s)) {
+			return;
+		}
 
-        final ProgressDialog dialog = ProgressDialog.show(this, "", "Calculating...", true);
-        final long n = Long.parseLong(s);
-        new AsyncTask<Void, Void, String>() {
+		final ProgressDialog dialog = ProgressDialog.show(this, "",
+				"Calculating...", true);
+		final long n = Long.parseLong(s);
+		new AsyncTask<Void, Void, String>() {
 
-            @Override
-            protected String doInBackground(Void... params) {
-                long result = 0;
-                long t = System.currentTimeMillis();
-                switch (FibonacciActivity.this.type.getCheckedRadioButtonId()) {
-                    case R.id.type_fib_jr:
-                        result = FibLib.fibJR(n);
-                        break;
-                    case R.id.type_fib_ji:
-                        result = FibLib.fibJI(n);
-                        break;
-                    case R.id.type_fib_nr:
-                        result = FibLib.fibNR(n);
-                        break;
-                    case R.id.type_fib_ni:
-                        result = FibLib.fibNI(n);
-                        break;
-                }
-                t = System.currentTimeMillis() - t;
-                return String.format("fib(%d)=%d in %d ms", n, result, t);
-            }
+			@Override
+			protected String doInBackground(Void... params) {
+				long result = 0;
+				long t = SystemClock.uptimeMillis();
+				switch (FibonacciActivity.this.type.getCheckedRadioButtonId()) {
+				case R.id.type_fib_jr:
+					result = FibLib.fibJR(n);
+					break;
+				case R.id.type_fib_ji:
+					result = FibLib.fibJI(n);
+					break;
+				case R.id.type_fib_nr:
+					result = FibLib.fibNR(n);
+					break;
+				case R.id.type_fib_ni:
+					result = FibLib.fibNI(n);
+					break;
+				}
+				t = SystemClock.uptimeMillis() - t;
+				return String.format("fib(%d)=%d in %d ms", n, result, t);
+			}
 
-            @Override
-            protected void onPostExecute(String result) {
-                dialog.dismiss();
-                FibonacciActivity.this.output.setText(result);
-            }
-        }.execute();
-    }
+			@Override
+			protected void onPostExecute(String result) {
+				dialog.dismiss();
+				FibonacciActivity.this.output.setText(result);
+			}
+		}.execute();
+	}
 }
